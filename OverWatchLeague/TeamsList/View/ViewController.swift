fa5bff535e5b429db6e2c2804e08bfef159f1e67
Vitor Pires <vitor.pires@thoughtworks.com>
2020-10-07 14:30:27 -0300

    Creates UIView

--- OverWatchLeague/TeamsList/View/ViewController.swift
+++ OverWatchLeague/TeamsList/View/ViewController.swift
@@ -6,109 +6,18 @@
 //
 
 import UIKit
-import EzImageLoader
 
 class ViewController: UIViewController {
 
-    private var presenter = TeamsPresenter()
-
-    private lazy var tableView: UITableView = {
-        let tableView = UITableView()
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.register(TableViewCell.self, forCellReuseIdentifier: "cell")
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.allowsSelection = false
-        return tableView
-    }()
-
-
-    private let containerView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = UIColor(named: "BackgroundColor")
-        return view
-    }()
-
-    private let titleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "OVERWATCH TEAMS DATABASE"
-        label.textColor = .white
-        label.font = .boldSystemFont(ofSize: 16)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
+    var presenter = TeamsPresenter()
+    lazy var teamList = TeamListView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        presenter.updateView()
-        setupViews()
-        updateTableView()
-        setupNavBar()
-    }
-
-    func updateTableView() {
-        presenter.reloadTableViewClosure = { [weak self] () in
-            DispatchQueue.main.async {
-                self?.tableView.reloadData()
-            }
-        }
-    }
-
-    func setupNavBar() {
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
-        imageView.contentMode = .scaleAspectFill
-        let image = UIImage(named: "Overwatch-Symbol")
-        imageView.image = image
-        navigationItem.titleView = imageView
-        navigationController?.navigationBar.isTranslucent = true
-        navigationController?.navigationBar.barStyle = .black
-    }
-
-    private func setupViews() {
-        view.addSubview(containerView)
-        containerView.addSubview(tableView)
-        updateConstraint()
-    }
-
-    func updateConstraint() {
-        NSLayoutConstraint.activate([
-            containerView.topAnchor.constraint(equalTo: view.topAnchor),
-            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            containerView.heightAnchor.constraint(equalTo: view.heightAnchor),
-
-            tableView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 80),
-            tableView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
-            tableView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 20)
-        ])
-    }
-}
-
-extension ViewController: UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return presenter.numberOfCells
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
-
-        let team = presenter.getcellPresenter(at: indexPath)
-        cell.nomeLabel.text = team.name.uppercased()
-        cell.apelidoLabel.text = team.abbreviatedName.uppercased()
-        cell.locationLabel.text = team.location.uppercased()
-
-        if let urlString = team.logo.main.png{
-            ImageLoader.get(urlString) {cell.logoImage.image = $0.image}
-        } else {
-            print("no image")
-        }
-        return cell
-    }
-
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return CGFloat(presenter.cellHeight)
+        view = teamList
+        teamList.setupViews()
+        teamList.setupNavBar(vc: self)
+        teamList.updateTableView()
     }
 }
 
